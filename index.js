0b7295e041541ad2c04b40e489984d47c148b237
rahulserver <rahulserver@gmail.com>
2021-11-23 18:58:27 +0530

    uploads

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ Above line would serve all files/folders inside of the 'b' directory
 And make them accessible through http://localhost:3000/a.
 */
 app.use(express.static(__dirname + '/public'));
-app.use('./uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
   // req.file is the `profile-file` file
