3df98b59456f938d9289a0adef82e95a2f5022e4
rahulserver <rahulserver@gmail.com>
2021-12-28 14:33:02 +0530

    script for excel xport

---
+++ scripts/firestoreToCsv.js
@@ -0,0 +1,23 @@
+var json2xls = require('json2xls');
+const fs = require('fs')
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccount.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+
+db.collection("bskEvents").where('district', '==', 'AHMEDABAD').where('state', '==', 'GUJARAT')
+  .get().then(snap => {
+  console.log("snap", snap.docs.length);
+  let docs = []
+  snap.docs.forEach(doc => {
+    docs.push(doc.data())
+  });
+  console.log("docs", docs)
+  var xls = json2xls(docs)
+  fs.writeFileSync('ahmedabad.xls', xls,'binary')
+})
+
