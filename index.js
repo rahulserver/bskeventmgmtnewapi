d317a634480a54268f90ee754215c9fbcab944c1
rahulserver <rahulserver@gmail.com>
2022-01-06 17:12:36 +0530

    push data to mongodb too

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var multer  = require('multer')
+const { MongoClient } = require('mongodb');
 var port = 3000;
+const uri = "mongodb+srv://root:root@cluster0.aaabc.mongodb.net/bskeventsdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
 var app = express()
 
@@ -22,9 +25,19 @@ And make them accessible through http://localhost:3000/a.
 app.use(express.static(__dirname + '/public'));
 app.use('/uploads', express.static('uploads'));
 
-app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+app.post('/profile-upload-single', upload.single('profile-file'), async function (req, res, next) {
   // req.file is the `profile-file` file
   // req.body will hold the text fields, if there were any
+  const collection = client.db('bskeventsdb').collection('bskEvents');
+  await collection.insertOne({
+    firestoreId: req.body.firestoreId,
+    name: req.body.name,
+    district: req.body.district,
+    state: req.body.state,
+    timestamp: req.body.timestamp,
+    path: req.file.path
+  });
+
   return res.send(req.file.path);
 })
 
@@ -37,9 +50,10 @@ app.post('/profile-upload-multiple', upload.array('profile-files', 12), function
     for(var i=0;i<req.files.length;i++){
         response += `<img src="${req.files[i].path}" /><br>`
     }
-    
+
     return res.send(response)
 })
-   
 
-app.listen(port,() => console.log(`Server running on port ${port}!\nClick http://localhost:3000/`))
+client.connect().then((err)=> {
+  app.listen(port,() => console.log(`Server running on port ${port}!\nClick http://localhost:3000/`))
+});
